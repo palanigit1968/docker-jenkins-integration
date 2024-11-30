3232e1b9b838d2db0e7792a93992ae6b4004a1b9
Palani <palselvam@gmail.com>
2024-11-30 16:36:06 +0530

    first commit

---
+++ src/main/java/com/palani/docker_jenkin_integration/App.java
@@ -0,0 +1,13 @@
+package com.palani.docker_jenkin_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
